75de98cda03e7788da0c5b2a2d951a98d8e2800a
Bekket <jackatwork@yandex.ru>
2023-04-01 23:43:50 +0400

    fix env config

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  env:{
+    TELEGRAM_KEY: process.env.TELEGRAM_KEY,
+  },
 }
 
 module.exports = nextConfig
