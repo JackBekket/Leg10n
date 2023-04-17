cc8e618b43bb36c560a7f6aa8e5f518f3aeb283f
Bekket <jackatwork@yandex.ru>
2023-04-18 00:49:29 +0400

    frontend addresses fix

--- dapp/src/pages/telesend.tsx
+++ dapp/src/pages/telesend.tsx
@@ -161,7 +161,7 @@ const Home: NextPage = () => {
           <Heading my={4}  fontSize='xl'>Encrypt message</Heading>
           <EncryptMessage 
                         currentAccount={currentAccount}
-                        addressContract='0x36Ed9bc9473F1316ec15BCca45f49f4481A63c5e'
+                        addressContract='0x8EC577a576Dd4783645c134572bF4bC486176aD1'
           />
         </Box>
        
